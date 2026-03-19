a150f1c073eeae4440ad4a61b40faef6c6cf30e2
Manjushareddy07 <144028049+Manjushareddy07@users.noreply.github.com>
2026-03-19 23:04:28 +0530

    Add functions to create policy and trigger claim

---
+++ frontend/script.js
@@ -0,0 +1,13 @@
+function createPolicy(){
+
+document.getElementById("policy").innerHTML =
+"Policy Created. Weekly Premium: ₹25 Coverage: ₹200 per disruption day";
+
+}
+
+function triggerClaim(){
+
+document.getElementById("claim").innerHTML =
+"Heavy rain detected. Claim approved. ₹200 payout initiated.";
+
+}
